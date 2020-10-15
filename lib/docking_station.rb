c7dab975a5e20fc903b5265507e3503a2dda4c18
Joseph <jpkershaw17@gmail.com>
2020-10-15 10:40:01 +0100

    Stage 15: Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,7 +10,7 @@ class DockingStation
   end
 
   def release_bike
-    fail "Error: No bike" if @bike_collection == []
+    fail "Error: No bike" if empty?
     # TODO: try with unless to see if it's empty
     @bike_collection.pop
 
@@ -23,7 +23,7 @@ class DockingStation
   end
 
   def dock(bike)
-    fail "Error: Bike already in place" if @bike_collection.length >= @capacity
+    fail "Error: Bike already in place" if full?
     @bike_collection.push(bike)
     # if @bikes_docked < @capacity
     #   @bikes_docked += 1
@@ -33,5 +33,12 @@ class DockingStation
     # end
     # if dockingstation holds a  @bike already then throw error message saying it's full
     # otherwise allow the bike to be docked
+  end
+  private
+  def full?
+    @bike_collection.length >= @capacity
+  end
+  def empty?
+    @bike_collection.empty?
   end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,9 +1,9 @@
-require './lib/docking_station'
-
-
-docking_station = DockingStation.new
-docking_station.dock(Bike.new)
-docking_station.dock(Bike.new)
-docking_station.dock(Bike.new)
-docking_station.dock(Bike.new)
-puts docking_station.bikes_docked
+# require './lib/docking_station'
+#
+#
+# docking_station = DockingStation.new
+# docking_station.dock(Bike.new)
+# docking_station.dock(Bike.new)
+# docking_station.dock(Bike.new)
+# docking_station.dock(Bike.new)
+# puts docking_station.bikes_docked
